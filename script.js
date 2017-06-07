e5579cb27b62f8b5700b5bf0af6c901197106672
ansaralishaik <ansarali.shaik@gmail.com>
2017-06-07 15:18:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var obj = {"data": [
+{
+	"ruleNumber" : 1,
+	"listTitle": "Lorem ipsum dolor ipsum1",
+	"listItems" : {
+		"list1" : "List item 1",
+		"list2" : "List item 2",
+		"list3" : "List item 3",
+	}
+}
+},
+{
+  "ruleNumber" : 2,
+  "listTitle": "Lorem ipsum dolor ipsum2",
+  "list1" : "List item 1",
+  "list2" : "List item 2",
+  "list3" : "List item 3",
+	
+}
+]}
+var divId = document.getElementById("jsondata")
+/* for(var i=0;i<obj.data.length;i++)
+for(var keys in obj.data[i]){
+ console.log(keys +"-->"+obj.data[i][keys]);
+ divId.innerHTML = divId.innerHTML + "<br/>"+ keys +"-->"+obj.data[i][keys];
+} */
